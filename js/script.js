20dbc1267203984c13da1ddbad14a4adfd762ef6
ElisabettaMirti <belmirti@gmail.com>
2024-05-15 16:15:06 +0200

    inserito ciclo while con creazione dei li per ogni elemento dell'array

--- js/script.js
+++ js/script.js
@@ -9,5 +9,15 @@ const listaSpesa = [ 'latte', 'pasta', 'cioccolato', 'scalogno', 'melanzane'];
 const UlElement = document.querySelector('ul');
 
 //Inserisco la variabile con l'indice
+
+let i = 0;
+
+
 //Creo il ciclo while per portare tutti gli elementi come <li> nell'html
+
+while (i = listaSpesa.length) {
+    const LiElement = document.createElement('li');
+    LiElement.append(listaSpesa[i]);
+    UlElement.appendChild(LiElement)
+}
 
