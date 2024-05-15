6295ad0f0d95051018975e6195f2bbc49486ba06
ElisabettaMirti <belmirti@gmail.com>
2024-05-15 16:10:06 +0200

    inserito pseudocodice di base e iniziato con creazione dell'array e richiamo del contenitore ul dal DOM

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+//Creo un array con gli elementi della lista della spesa
+
+const listaSpesa = [ 'latte', 'pasta', 'cioccolato', 'scalogno', 'melanzane'];
+
+//Creo una variabile per richiamare il contenitore nel DOM
+
+const UlElement = document.querySelector('ul');
+
+//Inserisco la variabile con l'indice
+//Creo il ciclo while per portare tutti gli elementi come <li> nell'html
+
